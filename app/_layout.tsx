08bee323b05607409b4ceef542f6165c0271ed9d
9Moses <esselmoses12@gmail.com>
2025-11-18 11:47:29 +0000

    auth clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,15 @@
 import SafeScreen from "@/components/SafeScreen";
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
 export default function RootLayout() {
   return (
+     <ClerkProvider tokenCache={tokenCache}>
     <SafeScreen>
-      <Stack screenOptions={{ headerShown: false }} />
+      {/* <Stack screenOptions={{ headerShown: false }} /> */}
+      <Slot />
     </SafeScreen>
+    </ClerkProvider>
   );
 }
